a425da688fb3ba0b94e06f2fa24df3a5ef6a001a
“ming” <123456789@qq.com>
2024-09-22 00:36:38 +0800

    修改

--- nodes/brightness_contrast_saturation.py
+++ nodes/brightness_contrast_saturation.py
@@ -13,7 +13,7 @@ def adjust_brightness_contrast_saturation(image, brightness, contrast, saturatio
         br = 0
     adjusted = cv2.addWeighted(image, 1, blank, 0.5, br)
     beta = np.mean(adjusted) * (1 - contrast)
-    adjusted2 = cv2.convertScaleAbs(adjusted, alpha=contrast)
+    adjusted2 = cv2.convertScaleAbs(adjusted, alpha=contrast, beta=beta)
     hsv = cv2.cvtColor(adjusted2, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
     s = cv2.addWeighted(s, saturation, 0, 0, 0)

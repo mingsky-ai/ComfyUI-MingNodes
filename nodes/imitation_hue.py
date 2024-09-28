35e1ac336acd205a99d7aab0ed1be47555c87d47
“ming” <123456789@qq.com>
2024-09-28 16:50:24 +0800

    修改

--- nodes/imitation_hue.py
+++ nodes/imitation_hue.py
@@ -64,7 +64,7 @@ class ImitationHueNode:
                 "imitation_image": ("IMAGE",),
                 "target_image": ("IMAGE",),
                 "strength": ("FLOAT", {"default": 1, "min": 0.1, "max": 1.0, "step": 0.1}),
-                "skin_protection": ("FLOAT", {"default": 0.5, "min": 0.1, "max": 1.0, "step": 0.1}),
+                "skin_protection": ("FLOAT", {"default": 0.3, "min": 0.1, "max": 1.0, "step": 0.1}),
             }
         }
 
